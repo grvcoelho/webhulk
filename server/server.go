8094dee548c9fb51b76e807b1f9add1b2a0ef892
Guilherme Coelho <guilhermervcoelho@gmail.com>
2017-10-14 12:57:58 -0300

    rename: add server

---
+++ server/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	iris "gopkg.in/kataras/iris.v8"
+
+	cfg "github.com/grvcoelho/caphook/config"
+	mdl "github.com/grvcoelho/caphook/middleware"
+)
+
+type Server struct {
+	app  *iris.Application
+	conf *cfg.Configuration
+}
+
+func (s *Server) Start() {
+	s.app.Run(iris.Addr(s.conf.Server.ListenOn))
+}
+
+func New(conf *cfg.Configuration) (*Server, error) {
+	logger := mdl.NewLogger()
+
+	app := iris.New()
+	app.Use(logger)
+
+	server := &Server{app, conf}
+
+	return server, nil
+}
