896bc0195364e8a02c91355615c416aa94966ac1
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-10-31 14:28:49 -0200

    message: add tests for type and store

--- pkg/database/db_test.go
+++ pkg/database/db_test.go
@@ -25,17 +25,40 @@ func TestDatabase(t *testing.T) {
 			Address: "postgres://postgres:postgres@database/webhulk?sslmode=disable",
 		})
 
-		store, err := NewWebhookStore(db)
+		webhookStore, err := NewWebhookStore(db)
 		assert.NoError(t, err)
 
 		w1, err := NewWebhook("events", "https://receiver.com", true)
 		assert.NoError(t, err)
 
-		_, err = store.Store(w1)
+		_, err = webhookStore.Store(w1)
 		assert.NoError(t, err, "Should store a webhook")
 
-		w2, ok := store.Retrieve(w1.ID)
+		w2, ok := webhookStore.Retrieve(w1.ID)
 		assert.True(t, ok)
 		assert.Equal(t, w1.ID, w2.ID)
+	})
+
+	t.Run("MessageStore", func(t *testing.T) {
+		db, _ := NewDatabase(&cfg.Database{
+			Address: "postgres://postgres:postgres@database/webhulk?sslmode=disable",
+		})
+
+		webhookStore, _ := NewWebhookStore(db)
+		messageStore, err := NewMessageStore(db)
+		assert.NoError(t, err)
+
+		w1, _ := NewWebhook("events", "https://receiver.com", true)
+		w1, _ = webhookStore.Store(w1)
+
+		m1, err := NewMessage(w1.ID, `{"event": "status_changed"}`)
+		assert.NoError(t, err)
+
+		_, err = messageStore.Store(m1)
+		assert.NoError(t, err, "Should store a message")
+
+		m2, ok := messageStore.Retrieve(m1.ID)
+		assert.True(t, ok)
+		assert.Equal(t, m1.ID, m2.ID)
 	})
 }
